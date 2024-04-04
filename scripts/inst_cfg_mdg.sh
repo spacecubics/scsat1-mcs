e14950ec7c7441da8d0cf0a1ea46f91cac048a3c
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2024-04-04 16:49:47 +0900

    scripts: Add install script for cfg and mdb
    This commit adds the install script for configuration files and
    mdb files.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

---
+++ scripts/inst_cfg_mdg.sh
@@ -0,0 +1,23 @@
+#/bin/sh -e
+
+(
+    read -r -p "Please input the working directory name: " work_dir
+    if [ ! -d ${work_dir} ]; then
+        echo "${work_dir} does not exist or is not a directory."
+        exit 1
+    fi
+
+    echo "Installing configuration and MDB files..."
+    cp -a $(dirname $0)/../etc ${work_dir}/
+    cp -a $(dirname $0)/../mdb ${work_dir}/
+
+    sed -i "s|mdb\/|${work_dir}\/mdb\/|g" ${work_dir}/etc/yamcs.*.yaml
+    sed -i "s|\/storage\/|${work_dir}\/storage\/|g" ${work_dir}/etc/yamcs.yaml
+
+    if [ ! -e ${work_dir}/storage ]; then
+        echo "Creating storage directory..."
+        mkdir ${work_dir}/storage
+    fi
+
+    echo "Installation finished."
+)
