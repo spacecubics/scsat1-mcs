555fd481c2e239c97ebd22da8a446b0dab484ee9
Yasushi SHOJI <yashi@spacecubics.com>
2024-08-31 21:56:04 +0900

    scripts: inst_cfg_mdb: Add die() function for better error handling
    In the previous commit, we've removed subshell for the script. the
    subshell along with -e makes the script to exit when any error occurs.
    
    Instead, add a die function to handle errors more effectively by
    providing specific error messages and exiting the script. This
    approach replaces the need to encapsulate the entire script in a
    subshell for error isolation.
    
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

--- scripts/inst_cfg_mdb.sh
+++ scripts/inst_cfg_mdb.sh
@@ -1,5 +1,12 @@
 #/bin/sh -e
 
+die()
+{
+	echo >&2
+	echo "$*" >&2
+	exit 1
+}
+
 if [ -z "$1" ]; then
   echo "Usage: $0 <directory-name>"
   exit 1
@@ -9,14 +16,14 @@ work_dir=$1
 mkdir -p "${work_dir}"
 
 echo "Create MDB files..."
-python $(dirname $0)/../py/create_xtce.py --data main
-python $(dirname $0)/../py/create_xtce.py --data adcs
-python $(dirname $0)/../py/create_xtce.py --data eps
-python $(dirname $0)/../py/create_xtce.py --data srs3
+python $(dirname $0)/../py/create_xtce.py --data main || die "main failed"
+python $(dirname $0)/../py/create_xtce.py --data adcs || die "adcs failed"
+python $(dirname $0)/../py/create_xtce.py --data eps  || die "eps failed"
+python $(dirname $0)/../py/create_xtce.py --data srs3 || die "srs3 failed"
 
 echo "Installing configuration and MDB files..."
-cp -a $(dirname $0)/../etc "${work_dir}"/
-cp -a $(dirname $0)/../mdb "${work_dir}"/
+cp -a $(dirname $0)/../etc "${work_dir}"/  || die "copy etc failed"
+cp -a $(dirname $0)/../mdb "${work_dir}"/  || die "copy mdb failed"
 
 sed -i "s|mdb\/|${work_dir}\/mdb\/|g" "${work_dir}"/etc/yamcs.*.yaml
 sed -i "s|\/storage\/|${work_dir}\/storage\/|g" "${work_dir}"/etc/yamcs.yaml
