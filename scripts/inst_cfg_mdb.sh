96f079ac78a235abd4482e79b9c1afb9fda5864d
Yasushi SHOJI <yashi@spacecubics.com>
2024-08-31 21:19:55 +0900

    scripts: inst_cfg_mdb: Remove subshell
    I don't know why it has it.  I don't think we need the
    subshell. Remove it.
    
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

--- scripts/inst_cfg_mdb.sh
+++ scripts/inst_cfg_mdb.sh
@@ -1,29 +1,27 @@
 #/bin/sh -e
 
-(
-    read -r -p "Please input the working directory name: " work_dir
-    if [ ! -d ${work_dir} ]; then
-        echo "${work_dir} does not exist or is not a directory."
-        exit 1
-    fi
+read -r -p "Please input the working directory name: " work_dir
+if [ ! -d ${work_dir} ]; then
+    echo "${work_dir} does not exist or is not a directory."
+    exit 1
+fi
 
-    echo "Create MDB files..."
-    python $(dirname $0)/../py/create_xtce.py --data main
-    python $(dirname $0)/../py/create_xtce.py --data adcs
-    python $(dirname $0)/../py/create_xtce.py --data eps
-    python $(dirname $0)/../py/create_xtce.py --data srs3
+echo "Create MDB files..."
+python $(dirname $0)/../py/create_xtce.py --data main
+python $(dirname $0)/../py/create_xtce.py --data adcs
+python $(dirname $0)/../py/create_xtce.py --data eps
+python $(dirname $0)/../py/create_xtce.py --data srs3
 
-    echo "Installing configuration and MDB files..."
-    cp -a $(dirname $0)/../etc ${work_dir}/
-    cp -a $(dirname $0)/../mdb ${work_dir}/
+echo "Installing configuration and MDB files..."
+cp -a $(dirname $0)/../etc ${work_dir}/
+cp -a $(dirname $0)/../mdb ${work_dir}/
 
-    sed -i "s|mdb\/|${work_dir}\/mdb\/|g" ${work_dir}/etc/yamcs.*.yaml
-    sed -i "s|\/storage\/|${work_dir}\/storage\/|g" ${work_dir}/etc/yamcs.yaml
+sed -i "s|mdb\/|${work_dir}\/mdb\/|g" ${work_dir}/etc/yamcs.*.yaml
+sed -i "s|\/storage\/|${work_dir}\/storage\/|g" ${work_dir}/etc/yamcs.yaml
 
-    if [ ! -e ${work_dir}/storage ]; then
-        echo "Creating storage directory..."
-        mkdir ${work_dir}/storage
-    fi
+if [ ! -e ${work_dir}/storage ]; then
+    echo "Creating storage directory..."
+    mkdir ${work_dir}/storage
+fi
 
-    echo "Installation finished."
-)
+echo "Installation finished."
