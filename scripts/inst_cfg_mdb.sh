747d888ec4d096150fb983e1f109ff721723c5d9
Yasushi SHOJI <yashi@spacecubics.com>
2024-09-01 00:47:17 +0900

    scripts/inst_cfg_mdb: Generate the header file only once
    Avoid repeatedly overwriting the header file by generating it just
    once.
    
    This alos allows us to remove mdb/.gitkeep which was introduced by
    edb05cc4cb9f1647e225c4b4cb7066dc4221acc2.
    
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

--- scripts/inst_cfg_mdb.sh
+++ scripts/inst_cfg_mdb.sh
@@ -18,14 +18,14 @@ mkdir -p "${work_dir}/mdb"
 echo "Create MDB files..."
 data=$(readlink -f $(dirname $0)/../py/tctm)
 create_xtce=$(readlink -f $(dirname $0)/../py/create_xtce.py)
-$create_xtce --verbose --header --name main --tm $data/main_tm.yaml --tc $data/main_tc.yaml --out "${work_dir}"/mdb/scsat1_main.xml || die "main failed"
-$create_xtce --verbose --header --name adcs --tm $data/adcs_tm.yaml --tc $data/adcs_tc.yaml --out "${work_dir}"/mdb/scsat1_adcs.xml || die "adcs failed"
-$create_xtce --verbose --header --name eps  --tm $data/eps_tm.yaml  --tc $data/eps_tc.yaml  --out "${work_dir}"/mdb/scsat1_eps.xml  || die "eps failed"
-$create_xtce --verbose --header --name srs3 --tm $data/srs3_tm.yaml --tc $data/srs3_tc.yaml --out "${work_dir}"/mdb/scsat1_srs3.xml || die "srs3 failed"
+$create_xtce --verbose --header "${work_dir}"/mdb/scsat1_header.xml || die "header failed"
+$create_xtce --verbose --name main --tm $data/main_tm.yaml --tc $data/main_tc.yaml --out "${work_dir}"/mdb/scsat1_main.xml || die "main failed"
+$create_xtce --verbose --name adcs --tm $data/adcs_tm.yaml --tc $data/adcs_tc.yaml --out "${work_dir}"/mdb/scsat1_adcs.xml || die "adcs failed"
+$create_xtce --verbose --name eps  --tm $data/eps_tm.yaml  --tc $data/eps_tc.yaml  --out "${work_dir}"/mdb/scsat1_eps.xml  || die "eps failed"
+$create_xtce --verbose --name srs3 --tm $data/srs3_tm.yaml --tc $data/srs3_tc.yaml --out "${work_dir}"/mdb/scsat1_srs3.xml || die "srs3 failed"
 
-echo "Installing configuration and MDB files..."
+echo "Installing configuration files from etc to ${work_dir}..."
 cp -a $(dirname $0)/../etc "${work_dir}"/  || die "copy etc failed"
-cp -a $(dirname $0)/../mdb "${work_dir}"/  || die "copy mdb failed"
 
 sed -i "s|mdb\/|${work_dir}\/mdb\/|g" "${work_dir}"/etc/yamcs.*.yaml
 sed -i "s|\/storage\/|${work_dir}\/storage\/|g" "${work_dir}"/etc/yamcs.yaml
