dc673c693fbf783b79d1aacfe288ec74e3a15c27
Yasushi SHOJI <yashi@spacecubics.com>
2024-08-31 21:33:22 +0900

    scripts: inst_cfg_mdb: Quote work_dir variable
    Without quotes, directory names with spaces, such as foo bar/, don't
    work.
    
    It's a rare case, but it's good practice to always quote user input
    variables.
    
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

--- scripts/inst_cfg_mdb.sh
+++ scripts/inst_cfg_mdb.sh
@@ -15,15 +15,15 @@ python $(dirname $0)/../py/create_xtce.py --data eps
 python $(dirname $0)/../py/create_xtce.py --data srs3
 
 echo "Installing configuration and MDB files..."
-cp -a $(dirname $0)/../etc ${work_dir}/
-cp -a $(dirname $0)/../mdb ${work_dir}/
+cp -a $(dirname $0)/../etc "${work_dir}"/
+cp -a $(dirname $0)/../mdb "${work_dir}"/
 
-sed -i "s|mdb\/|${work_dir}\/mdb\/|g" ${work_dir}/etc/yamcs.*.yaml
-sed -i "s|\/storage\/|${work_dir}\/storage\/|g" ${work_dir}/etc/yamcs.yaml
+sed -i "s|mdb\/|${work_dir}\/mdb\/|g" "${work_dir}"/etc/yamcs.*.yaml
+sed -i "s|\/storage\/|${work_dir}\/storage\/|g" "${work_dir}"/etc/yamcs.yaml
 
-if [ ! -e ${work_dir}/storage ]; then
+if [ ! -e "${work_dir}"/storage ]; then
     echo "Creating storage directory..."
-    mkdir ${work_dir}/storage
+    mkdir "${work_dir}"/storage
 fi
 
 echo "Installation finished."
