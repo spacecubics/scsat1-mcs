3191efa5ee1b55e5f6be5b4fda183c73993cbe6d
Riho Natsushima <nasshi7224@gmail.com>
2024-10-25 15:57:15 +0900

    scripts: Add support for Raspberry Pi Pico
     - Added Pico's create_xtce
    
    Signed-off-by: Riho Natsushima <nasshi7224@gmail.com>

--- scripts/inst_cfg_mdb.sh
+++ scripts/inst_cfg_mdb.sh
@@ -23,6 +23,7 @@ $create_xtce --verbose --name main --tm $data/main_tm.yaml --tc $data/main_tc.ya
 $create_xtce --verbose --name adcs --tm $data/adcs_tm.yaml --tc $data/adcs_tc.yaml --out "${work_dir}"/mdb/scsat1_adcs.xml || die "adcs failed"
 $create_xtce --verbose --name eps  --tm $data/eps_tm.yaml  --tc $data/eps_tc.yaml  --out "${work_dir}"/mdb/scsat1_eps.xml  || die "eps failed"
 $create_xtce --verbose --name srs3 --tm $data/srs3_tm.yaml --tc $data/srs3_tc.yaml --out "${work_dir}"/mdb/scsat1_srs3.xml || die "srs3 failed"
+$create_xtce --verbose --name pico --tm $data/pico_tm.yaml --tc $data/pico_tc.yaml --out "${work_dir}"/mdb/scsat1_pico.xml || die "pico failed"
 
 echo "Installing configuration files from etc to ${work_dir}..."
 cp -a $(dirname $0)/../etc "${work_dir}"/  || die "copy etc failed"
